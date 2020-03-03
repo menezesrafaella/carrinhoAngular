8ce6bb669724ac22ecf0bab375f32f6b2df26f7b
Treinamento <Treinamento@RD723897.raiadrogasil.com>
2020-03-03 17:26:39 -0300

    somente html funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarrinhoComponent } from './components/carrinho/carrinho.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarrinhoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
